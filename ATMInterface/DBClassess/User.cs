4a712a2b8755879c3343d3011afa3d2ceda46808
Xeno_murf <kseniia.vilkhova@ukma.edu.ua>
2022-12-04 18:31:34 +0200

    added DB and classes

---
+++ ATMInterface/DBClassess/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMInterface.DBClassess
+{
+    public class User
+    {
+        public int Id_user { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Birth_date { get; set; }
+    }
+}
