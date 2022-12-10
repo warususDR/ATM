c17422d7f58f24809995f7465ba76e3d241f3035
Anastasiia Parkhomenko <anastasia031100@gmail.com>
2022-12-11 00:29:07 +0200

    bug fix

--- ATMInterface/Core/LogSys.cs
+++ ATMInterface/Core/LogSys.cs
@@ -75,7 +75,7 @@ namespace ATM
         public eHeader Header => _header;
         private eHeader _header;
 
-        internal void ApplyData(Data userData)//shit
+        internal void ApplyData(Data userData)
         {
             if(userData.Password != null) { this._data.Password = userData.Password; }
             if(userData.СardNumber != null) { this._data.СardNumber = userData.СardNumber; }
