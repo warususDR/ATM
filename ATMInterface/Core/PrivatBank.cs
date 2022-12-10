e62bb277d870a7863e6a01b88806fb98c727932f
Anastasiia Parkhomenko <anastasia031100@gmail.com>
2022-12-10 17:04:13 +0200

    new functionality and banks added

---
+++ ATMInterface/Core/PrivatBank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class ePrivatBank : eBank
+    {
+        static private string bankCode = "22";
+        public ePrivatBank(eCommutator _commutator)
+            : base(bankCode, _commutator)
+        {}
+    }
+}
